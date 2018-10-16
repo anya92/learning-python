4ef0717c08adb683372ec404c7dd61ed0a196b26
Ania <aniabrajtkopf@gmail.com>
2018-10-16 15:19:08 +0200

    added numbers and operators

---
+++ 1.Numbers/numbers.py
@@ -0,0 +1,12 @@
+"""
+Numeric types in Python:
+  - int
+  - float
+  - complex
+"""
+
+1 # int
+3.2 # float
+
+print(type(-1))  # <class 'int'>
+print(type(0.5))  # <class 'float'>
---
+++ 1.Numbers/operators.py
@@ -0,0 +1,10 @@
+"""
+operator |      name      | example
+   +     |    addition    | 1 + 2 # 3
+   -     |   subtraction  | 15 - 4 # 11
+   *     | multiplication | 3 * 2 # 6
+   /     |    division    | 15 / 3 # 5.0 (always returns a float)
+   %     |     modulus    | 7 % 2 # 1
+   **    | exponentiation | 2 ** 3 # 8
+   //    | floor division | 15 // 3 # 5 (always rounds down)
+"""
