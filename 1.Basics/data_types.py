3f2f077ebbdf8ff36d2b15c84d9820a59030481a
Ania <aniabrajtkopf@gmail.com>
2018-10-22 18:37:41 +0200

    added data types

---
+++ 1.Basics/data_types.py
@@ -0,0 +1,12 @@
+"""
+  Data types:
+    - strings (str) - sequences of unicode characters, surrounded by either single quotation marks, or double quotation marks, e.g. 'Hello'.
+    - numbers:
+      - integers (int) - whole numbers, positive or negative, without decimals, e.g. 2, -10
+      - floats (float) - numbers, positive or negative, containing one or more decimals, e.g. 0.5, -13.43
+    - booleans (bool) - may have one of two values, True or False
+    - lists (list) - collections which are ordered and changeable,  written with square brackets, e.g. [1, 2, 3],
+    - dictionaries (dict) - collections of key-values pairs, written with curly brackets, e.g. { "first_name": "Andy", "age": 28 }
+    - tuples (tuple) - collections which are ordered and unchangeable, written with round brackets, e.g. ("red", "black", "blue")
+    - sets (set) - collections which are unordered and unindexed, written with curly brackets, e.g. { "Tokyo", "Berlin", "Dallas" }
+"""
