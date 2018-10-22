daba3c4c67252e5711b8968e0ead2b71d93ce156
Ania <aniabrajtkopf@gmail.com>
2018-10-22 15:23:49 +0200

    moved files to Basics folder

--- 1.Basics/numbers_and_operators.py
+++ 1.Basics/numbers_and_operators.py
@@ -1,4 +1,17 @@
 """
+Numeric types in Python:
+  - int
+  - float
+  - complex
+"""
+
+1 # int
+3.2 # float
+
+print(type(-1))  # <class 'int'>
+print(type(0.5))  # <class 'float'>
+
+"""
 operator |      name      | example
    +     |    addition    | 1 + 2 # 3
    -     |   subtraction  | 15 - 4 # 11
@@ -6,5 +19,5 @@ operator |      name      | example
    /     |    division    | 15 / 3 # 5.0 (always returns a float)
    %     |     modulus    | 7 % 2 # 1
    **    | exponentiation | 2 ** 3 # 8
-   //    | floor division | 15 // 3 # 5 (always rounds down)
+   //    | floor division | 15 // 3 # 5 (returns int, always rounds down)
 """
