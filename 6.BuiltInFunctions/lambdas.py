6251b9109188a40084928d895347f815fb08cabd
Ania <aniabrajtkopf@gmail.com>
2018-11-04 16:55:43 +0100

    added lambdas

---
+++ 6.BuiltInFunctions/lambdas.py
@@ -0,0 +1,9 @@
+# lambda function:
+# - a small anonymous function,
+# - can take any number of arguments, but can only have one expression
+
+# lambda arguments : expression
+
+sum_two_numbers = lambda a, b: a + b
+
+print(sum_two_numbers(3, 4))  # 7
