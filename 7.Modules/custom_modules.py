ce58164116d3b0299315dfef1b4e75174109a1d2
Ania <aniabrajtkopf@gmail.com>
2018-11-08 09:31:01 +0100

    added custom_modules

---
+++ 7.Modules/custom_modules.py
@@ -0,0 +1,7 @@
+import functions1
+from functions2 import add
+
+functions1.say_hello('Jenny')  # 'Hello, Jenny'
+functions1.say_goodbye('Matt')  # 'Goodbye, Matt'
+
+print(add(3, 6))  # 9
---
+++ 7.Modules/functions1.py
@@ -0,0 +1,6 @@
+def say_hello(name):
+    print(f"Hello, {name}")
+
+
+def say_goodbye(name):
+    print(f"Goodbye, {name}")
