85e50a9f99161575c1afceb90336ecb79d42402b
Ania <aniabrajtkopf@gmail.com>
2018-11-13 09:04:38 +0100

    added requests with params

--- HTTPRequests/http_requests.py
+++ HTTPRequests/http_requests.py
@@ -1,20 +1,39 @@
 import requests
 
 url = "https://google.com"
-res = requests.get(url)
+response = requests.get(url)
 
 print(
-    f"Your request to {url} came back with status code: {res.status_code}")
+    f"Your request to {url} came back with status code: {response.status_code}")
 
 # print(response.headers)
 # print(response.text)
 
-url_2 = "https://api.github.com/events"
+# requesting JSON
 
-response = requests.get(url_2, headers={
+url_2 = "https://icanhazdadjoke.com/"
+
+response_2 = requests.get(url_2, headers={
+    # 'Accept': "text/plain"
     'Accept': "application/json"
 })
 
-data = response.json()
+print(response_2.text)
 
-print(type(data))  # <class 'list'>
+data = response_2.json()
+
+print(type(data))  # <class 'dict'>
+
+print(data["joke"])
+
+# requests with params
+
+url_3 = "https://icanhazdadjoke.com/search"
+
+response_3 = requests.get(
+    url_3,
+    headers={'Accept': "application/json"},
+    params={'term': 'school', 'limit': 1}
+)
+
+print(response_3.json()["results"])
