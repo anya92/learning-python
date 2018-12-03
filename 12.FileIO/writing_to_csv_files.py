cc9f5258e760a260d0865a5a6ac1dd38b230854c
Ania <aniabrajtkopf@gmail.com>
2018-12-03 09:55:15 +0100

    added writing to csv files using DictWriter

--- 12.FileIO/writing_to_csv_files.py
+++ 12.FileIO/writing_to_csv_files.py
@@ -1,6 +1,6 @@
 # using lists
 
-from csv import reader, writer
+from csv import reader, writer, DictReader, DictWriter
 
 with open("people.csv", "w") as file:
     csv_writer = writer(file)
@@ -14,3 +14,28 @@ with open("countries.csv") as file:
         csv_writer = writer(file)
         for row in csv_reader:
             csv_writer.writerow([row[3], row[0]])
+
+# using DictWriter
+
+with open("animals.csv", "w") as file:
+    headers = ["Name", "Type", "Age"]
+    csv_writer = DictWriter(file, fieldnames=headers)
+    csv_writer.writeheader()
+    csv_writer.writerow({
+        "Name": "Mr Puppy",
+        "Type": "Dog",
+        "Age": 2
+    })
+
+with open("countries.csv") as file:
+    csv_reader = DictReader(file)
+    with open("country_coords.csv", "w") as file:
+        headers = ["Country Name", "Lat", "Lon"]
+        csv_writer = DictWriter(file, fieldnames=headers)
+        csv_writer.writeheader()
+        for row in csv_reader:
+            csv_writer.writerow({
+                "Country Name": row["country"],
+                "Lat": row["latitude"],
+                "Lon": row["longitude"]
+            })
